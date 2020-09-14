727f38186470ee672817f292d125cf4f6aba4cda
Alexey <alexey6486@gmail.com>
2020-09-14 10:05:18 +0300

    Updates

---
+++ precache-manifest.8a35557dbbc3ce5574136a978fa129cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf293e7583053563c602130a9ff8e65",
+    "url": "/future-test-chernov/index.html"
+  },
+  {
+    "revision": "2929fb3f3beff3b95b03",
+    "url": "/future-test-chernov/static/css/main.584a340e.chunk.css"
+  },
+  {
+    "revision": "91cbe17c1f644a74abc3",
+    "url": "/future-test-chernov/static/js/2.6db725ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/future-test-chernov/static/js/2.6db725ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2929fb3f3beff3b95b03",
+    "url": "/future-test-chernov/static/js/main.14694282.chunk.js"
+  },
+  {
+    "revision": "18aac6195590dac535f7",
+    "url": "/future-test-chernov/static/js/runtime-main.48603634.js"
+  }
+]);
