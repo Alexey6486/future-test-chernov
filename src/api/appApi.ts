ce5c548659f869c2afdbfb10be738ae8b2688a46
Alexey <alexey6486@gmail.com>
2020-09-15 12:07:02 +0300

    minor_fix

--- src/api/appApi.ts
+++ src/api/appApi.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://www.filltext.com/',
+
 });
 
 export interface ServerResponseObjectType extends Record<string, any> {
